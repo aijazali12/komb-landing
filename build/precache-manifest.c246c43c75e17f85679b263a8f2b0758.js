3654811f37d4fcfe8b541e7b059c3a3d028a4519
Aijaz <aijaz070110@gmail.com>
2022-06-17 14:39:07 +0500

    landing page build

---
+++ build/precache-manifest.c246c43c75e17f85679b263a8f2b0758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a59bd57120ccc8cc67c",
+    "url": "/static/css/main.166dfc4f.chunk.css"
+  },
+  {
+    "revision": "9a59bd57120ccc8cc67c",
+    "url": "/static/js/main.9a59bd57.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "4f7cf36b43351e8d8ed4",
+    "url": "/static/css/2.e47ed2d1.chunk.css"
+  },
+  {
+    "revision": "4f7cf36b43351e8d8ed4",
+    "url": "/static/js/2.4f7cf36b.chunk.js"
+  },
+  {
+    "revision": "6911db604fedf1b3b856",
+    "url": "/static/js/runtime~main.6911db60.js"
+  },
+  {
+    "revision": "1adc022c1005a55c28103974313383f9",
+    "url": "/index.html"
+  }
+];
