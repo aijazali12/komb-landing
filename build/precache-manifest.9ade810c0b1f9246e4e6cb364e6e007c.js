bb53e77b3416afae4917ee0e82cddf2f83adccab
Aijaz <aijaz070110@gmail.com>
2022-06-17 19:26:14 +0500

    build uploaded

---
+++ build/precache-manifest.9ade810c0b1f9246e4e6cb364e6e007c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "32328062ff01f00f5b3a",
+    "url": "/static/css/main.166dfc4f.chunk.css"
+  },
+  {
+    "revision": "32328062ff01f00f5b3a",
+    "url": "/static/js/main.32328062.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "4f7cf36b43351e8d8ed4",
+    "url": "/static/css/2.e47ed2d1.chunk.css"
+  },
+  {
+    "revision": "4f7cf36b43351e8d8ed4",
+    "url": "/static/js/2.4f7cf36b.chunk.js"
+  },
+  {
+    "revision": "6911db604fedf1b3b856",
+    "url": "/static/js/runtime~main.6911db60.js"
+  },
+  {
+    "revision": "350bad0a73fdd533d5f76673d15fab64",
+    "url": "/index.html"
+  }
+];
